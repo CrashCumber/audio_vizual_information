c2ae05b5495106d814b99e2a824b56a1cbbdc129
Valentina Askretkova <valya.as@yandex.ru>
2021-02-19 11:28:34 +0300

    add report and new images

--- laba_1/test_resize.py
+++ laba_1/test_resize.py
@@ -4,27 +4,23 @@ from laba_1.utils import *
 
 def upsampling_test(img):
     img_up = upsampling(img, M)
-    img_up.show()
-    img_up.save("result_images/img_up")
+    img_up.save("result_images/sampling/img_up.png")
 
 
 def downsampling_test(img):
     img_down = downsampling(img, N)
-    img_down.show()
-    img_down.save("result_images/img_down")
+    img_down.save("result_images/sampling/img_down.png")
 
 
 def resampling_test(img):
     k = M / N
     img_samp = resampling(img, k)
-    img_samp.show()
-    img_samp.save("result_images/img_samp_one")
+    img_samp.save("result_images/sampling/img_samp_one.png")
 
 
 def resampling_two_step_test(img):
     img_samp = downsampling(upsampling(img, M), N)
-    img_samp.show()
-    img_samp.save("result_images/img_samp_two")
+    img_samp.save("result_images/sampling/img_samp_two.png")
 
 
 img_name = input("Enter name of image:")
